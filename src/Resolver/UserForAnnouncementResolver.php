7614100e1a044449936ec579c6ba04862a3fe9ee
lukas <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>
2022-04-17 18:08:57 +0200

    Add form for add announcement

---
+++ src/Resolver/UserForAnnouncementResolver.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Resolver;
+
+use mysql_xdevapi\Exception;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class UserForAnnouncementResolver implements UserForAnnouncementResolverIntefrace
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function getUsernameForAnnouncement(): UserInterface
+    {
+        $user = $this->security->getUser();
+
+        if(null === $user) {
+            throw new \Exception('Can not found user');
+        }
+
+        return $user;
+    }
+}
